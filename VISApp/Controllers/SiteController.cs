ca3c2cf326ff2712c9c84bc1021e4d0d52704b0c
Saransh Kumar <saransh.a.kumar@accenture.com>
2024-07-15 15:49:01 +0530

    Image stored in local UserImg folder.

--- VISApp/Controllers/SiteController.cs
+++ VISApp/Controllers/SiteController.cs
@@ -4,6 +4,12 @@ namespace Curate.AI.Controllers
 {
     public class SiteController : Controller
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public SiteController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
@@ -11,10 +17,25 @@ namespace Curate.AI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Index(IFormFile formFile)
+        public IActionResult Index(IFormFile file)
         {
+            if (file != null)
+            {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName); //Random name to image file
+                    string productPath = Path.Combine(wwwRootPath, @"userImg");
 
-            return View("Result");
+                    using (var fileStream = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    ViewData["img"] = @"wwwroot\UserImg\"+filename;
+                    return View("Result");
+                }
+            }
+            return View();
         }
     }
 }
